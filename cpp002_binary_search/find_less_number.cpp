d1e4819cd8895ee9296e5a66ad2d4723aa0e7cbc
hcUltra <18246193960@163.com>
2024-05-08 07:23:31 +0800

    2024.5.8 binary search find equals numbers range [l,r]

---
+++ cpp002_binary_search/find_less_number.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 在有序数组中，找到最后一个 <= target 的位置并返回，如果没有满足条件的位置则返回-1
+ * */
+int BinarySearch(int arr[], int length, int target)
+{
+    int l = 0, r = length - 1, mid = 0, ans = -1;
+    while(l <= r)
+    {
+        mid = l + ((r - l) >> 1);
+        if(arr[mid] <= target)
+        {
+            l = mid + 1;
+            ans = mid;
+        } else
+        {
+            r = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 4, 4, 5, 6, 7, 8};
+    cout << "查找到的首个>= " << arr[3] << "下标：" << (BinarySearch(arr, sizeof(arr)/sizeof(int), arr[3])) << endl;
+    return 0;
+}
